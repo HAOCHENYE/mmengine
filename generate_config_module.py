4e1bee870126640b8dade352baaf370dfbc38720
HAOCHENYE <21724054@zju.edu.cn>
2022-09-14 14:47:37 +0800

    first commit

--- generate_config_module.py
+++ generate_config_module.py
@@ -3,13 +3,12 @@ import importlib
 import inspect
 from io import TextIOWrapper
 import os.path as osp
+import os
 import typing
 import builtins
-from chanfig import Config
 from mmengine.utils import get_installed_path, mkdir_or_exist
 from mmengine.registry import Registry
-from mmcv.utils import register_all_modules
-
+from mmengine.utils.dl_utils import register_all_modules
 
 class Require(object):
     pass
@@ -17,46 +16,52 @@ class Require(object):
 
 def get_all_arguments(cls,
                       args,
-                      kwonlyargs):
+                      kwonlyargs,
+                      varargs=None,
+                      varkwargs=None):
     if cls in builtins.__dict__.values():
         return
 
+    full_args = inspect.getfullargspec(cls.__init__)
+    _args = full_args.args
+    _kwonlyargs = full_args.kwonlyargs
+    _default_args = full_args.defaults
+    _default_args = [] if _default_args is None else _default_args
+    _default_kwargs = full_args.kwonlydefaults
+    _default_kwargs = {} if _default_kwargs is None else _default_kwargs
+    varkwargs = full_args.varkw
+    varargs = full_args.varargs
+
+    args_iter = iter(_args)
+
+    for _ in range(len(_args) - len(_default_args)):
+        arg = next(args_iter)
+        args.setdefault(arg, Require())
+
+    for idx in range(len(_default_args)):
+        arg = next(args_iter)
+        args.setdefault(arg, _default_args[idx])
+
+    for kwonlyarg in _kwonlyargs:
+        kwonlyargs.setdefault(
+            kwonlyarg, _default_kwargs.get(kwonlyarg, Require()))
+
+    if varkwargs is None and varargs is None:
+        return
+
     for base in (cls.__base__, ):
-        full_args = inspect.getfullargspec(base.__init__)
-        _args = full_args.args
-        _kwonlyargs = full_args.kwonlyargs
-        _default_args = full_args.defaults
-        _default_args = [] if _default_args is None else _default_args
-        _default_kwargs = full_args.kwonlydefaults
-        _default_kwargs = {} if _default_kwargs is None else _default_kwargs
-
-
-
-        args_iter = iter(_args)
-        for _ in range(len(_args) - len(_default_args)):
-            arg = next(args_iter)
-            args.setdefault(arg, Require())
-
-        for idx in range(len(_default_args)):
-            arg = next(args_iter)
-            args.setdefault(arg, _default_args[idx])
-
-
-        for kwonlyarg in _kwonlyargs:
-            kwonlyargs.setdefault(
-                kwonlyarg, _default_kwargs.get(kwonlyarg, Require()))
-        get_all_arguments(base, args, kwonlyargs)
+        get_all_arguments(base, args, kwonlyargs, varargs, varkwargs)
 
 
 def generate_class(cls, file=None):
     full_args = inspect.getfullargspec(cls.__init__)
-    # args = full_args.args
-    # defaults = full_args.defaults if full_args.defaults is not None else []
-    # defaults = []
-    # kwonlyargs = full_args.kwonlyargs
-    # kwonlydefaults = full_args.kwonlydefaults
-    # annotations = full_args.annotations
+
     args = OrderedDict()
+    default_args = OrderedDict()
+    for key, value in args:
+        if isinstance(value, Require):
+            default_args[key] = args.pop(key)
+    args.update(default_args)
     kwonlyargs= OrderedDict()
     defaults = OrderedDict()
     get_all_arguments(cls, args, kwonlyargs)
@@ -65,9 +70,6 @@ def generate_class(cls, file=None):
 
     class_name = cls.__name__
     module_name = cls.__module__
-    bases = cls.__base__
-    if not isinstance(bases, (tuple)):
-        bases = (bases, )
     indent = ' ' * 4
 
     def write_import(file: TextIOWrapper):
@@ -79,7 +81,6 @@ def generate_class(cls, file=None):
         module_name_list = module_name.split('.')
         if len(module_name_list) > 1:
             module_name_list[1] = 'config_module'
-        new_module_name = ''.join(module_name_list)
         file.write('\n')
         file.write('\n')
 
@@ -87,6 +88,7 @@ def generate_class(cls, file=None):
         attrs = []
         # class ClassName(Config):
         file.write(f'class {class_name}(Config):\n')
+        file.write(f"{indent}type: '{class_name}'\n")
         # def __init__(self):
         file.write(f'{indent}def __init__(\n')
         args_iter = iter(args)
@@ -100,6 +102,8 @@ def generate_class(cls, file=None):
                            f',\n')
                 attrs.append(arg)
             else:
+                if isinstance(value, str):
+                    value = f"'{value}'"
                 file.write(f'{indent * 2}{arg}'
                            f'={value}'
                            # f': {annotation}'
@@ -122,65 +126,78 @@ def generate_class(cls, file=None):
                 file.write(f'{indent * 2}{key}'
                            # f': {annotation},'
                            f',\n')
-                attrs.append(kwonlyarg)
+                attrs.append(key)
             else:
+                if isinstance(value, str):
+                    value = f"'{value}'"
                 file.write(f'{indent * 2}{key}'
                            f'={value}'
                            # f': {annotation},'
                            f',\n')
                 attrs.append(key)
 
-
-
         if varkw is not None:
             file.write(f'{indent * 2}**{varkw}):\n')
         else:
             file.write(f'):\n')
 
-        for attr in attrs:
-            if attr == 'self':
-                continue
-            file.write(f'{indent * 2}self.{attr} = {attr}\n')
+        file.write(f'{indent * 2}super().__init__()\n')
+        if len(attrs) == 1:
+            file.write(f'{indent * 2}pass\n')
+        else:
+            for attr in attrs:
+                if attr == 'self':
+                    continue
+                file.write(f'{indent * 2}self.{attr} = {attr}\n')
 
-    def get_annotation(arg):
-        annotation = annotations.get(arg, typing.Any)
-        try:
-            annotation = annotation.__name__
-            annotation
-        except:
-            annotation = f'typing.{annotation._name}'
-        return annotation
-
-    write_import(file)
+    # def get_annotation(arg):
+    #     annotation = annotations.get(arg, typing.Any)
+    #     try:
+    #         annotation = annotation.__name__
+    #         annotation
+    #     except:
+    #         annotation = f'typing.{annotation._name}'
+    #     return annotation
+    if file.tell() == 0:
+        write_import(file)
     define_class(file)
 
 
-def config_from_registry(registry: Registry, mmcv_path: str):
+def config_from_registry(registry: Registry, module_path: str):
     for name, cls in registry.module_dict.items():
         module_name = cls.__module__
-        if not module_name.startswith('mmcv'):
+        if not module_name.startswith('mmengine'):
             continue
         module_name_list = module_name.split('.')
         module_name_list = module_name_list[1:]
         module_name_list.insert(0, 'config_module')
         module_package = '/'.join(module_name_list[:-1])
-        module_package = osp.join(mmcv_path, module_package)
+        module_package = osp.join(module_path, module_package)
         module_file = '/'.join(module_name_list)
-        module_file = osp.join(mmcv_path, module_file)
+        module_file = osp.join(module_path, module_file)
         mkdir_or_exist(module_package)
-        with open(osp.join(module_package, '__init__.py'), 'w') as f:
-            f.write('from . import *\n')
-        with open(module_file + '.py', 'w') as f:
+
+        with open(module_file + '.py', 'a+') as f:
             generate_class(cls, f)
 
 
 if __name__ == '__main__':
     register_all_modules()
 
-    mmcv_path = get_installed_path('mmcv')
-    all_registry = importlib.import_module('mmcv.registry')
-    config_path = osp.join(mmcv_path, 'config_module')
+    module_path = get_installed_path('mmengine')
+    all_registry = importlib.import_module('mmengine.registry')
+    config_path = osp.join(module_path, 'config_module')
     mkdir_or_exist(config_path)
     for value in all_registry.__dict__.values():
         if isinstance(value, Registry):
-            config_from_registry(value, mmcv_path)
+            config_from_registry(value, module_path)
+
+    for root_dir, sub_dir, file in os.walk(config_path):
+        with open(osp.join(root_dir, '__init__.py'), 'w') as f:
+            for module in sub_dir:
+                f.write(f'from .{module} import *\n')
+
+            for module in file:
+                if module.startswith('__init__'):
+                    continue
+                f.write(f'from .{module.rstrip(".py")} import *\n')
